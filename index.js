6da3af6be548ab684752b3072c47f25d595d0d2d
Storm <storm.sarracino01@gmail.com>
2021-06-09 01:33:56 -0500

    commit tree ed45cb20227988a1fcb2386a05b7f74b0cfda277

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random()) + 1;
+Number.isInteger(random);
+
+const num3 = 14;
+const num4 = 5;
+let mod = num3 % num4;
+
+let max = Math.max(16, 17, 18, 19, 20);
